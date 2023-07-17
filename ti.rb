88e5154c56d2f309efb1dc24e0c9ee166fe818d0
Dylan Waits <dylan@waits.io>
2023-07-17 10:56:08 -0700

    Make race selection optional

--- ti.rb
+++ ti.rb
@@ -2,11 +2,13 @@ seed = ARGV[0]
 players = ARGV[1..-1]
 srand(Integer(seed))
 
-race_count = players.length + 3
-races = File.read("ti_races.txt").split("\n\n").map do |race|
-  race.split("\n").first
-end.shuffle.take(race_count)
-
 puts "Draft order: " + players.shuffle.join(" ")
-puts "Available factions:\n"
-puts races.join("\n")
+
+if ENV["RACES"]
+  race_count = players.length + 3
+  races = File.read("ti_races.txt").split("\n\n").map do |race|
+    race.split("\n").first
+  end.shuffle.take(race_count)
+  puts "Available factions:\n"
+  puts races.join("\n")
+end
